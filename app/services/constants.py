3b82b7c91bcb1899a73104834c78217a4e34bc17
Serge Balyaba <erges699@gmail.com>
2022-12-27 09:46:55 +0300

    Create constants.py

---
+++ app/services/constants.py
@@ -0,0 +1,11 @@
+"""Константы."""
+
+# for google spreadsheet_body
+SPREDSHEET_REPORT_TITLE = 'Отчет по проектам на'
+SPREDSHEET_REPORT_LOCALE = 'ru_RU'
+SPREDSHEET_SHEET_PROPERTIES = {
+  'sheetType': 'GRID', 
+  'sheetId': 0, 
+  'title': 'Отчет', 
+  'gridProperties': {'rowCount': 100, 'columnCount': 4}
+}
