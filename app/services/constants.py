a42a218618ae650f8b1de3ffa218fc809e54117d
Serge Balyaba <erges699@gmail.com>
2022-12-27 10:19:43 +0300

    Update constants.py

--- app/services/constants.py
+++ app/services/constants.py
@@ -1,11 +1,13 @@
-"""Константы."""
+"""Константы.
+"""
+
 
 # for google spreadsheet_body
 SPREDSHEET_REPORT_TITLE = 'Отчет по закрытым проектам QRkot на'
 SPREDSHEET_REPORT_LOCALE = 'ru_RU'
 SPREDSHEET_SHEET_PROPERTIES = {
-  'sheetType': 'GRID', 
-  'sheetId': 0, 
-  'title': 'Отчет', 
-  'gridProperties': {'rowCount': 100, 'columnCount': 4}
+    'sheetType': 'GRID',
+    'sheetId': 0,
+    'title': 'Отчет',
+    'gridProperties': {'rowCount': 100, 'columnCount': 4}
 }
